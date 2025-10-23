00563f36c8b4bb8a424bbc3de9b2a12e86379ab3
Jonathan-Hobbs <jonathan_hobbs@byui.edu>
2025-10-23 16:56:08 -0600

    arrays practice

--- ponder/arrays.js
+++ ponder/arrays.js
@@ -5,3 +5,35 @@ function listTemplate(step) {
 }
 const stepsHtml = steps.map(listTemplate) // use map to convert the list from strings to HTML
 document.querySelector("#myList").innerHTML = stepsHtml.join("") // set the innerHTML
+
+const grades = ["A", "B", "A", "C"]
+function convertGradesToPoints(grade) {
+  let points = 0;
+  switch (grade) {
+    case "A":
+      points = 4;
+      break;
+    case "B":
+      points = 3;
+      break;
+    case "C":
+      points = 2;
+      break;
+  }
+  return points;
+}
+
+const gpaPoints = grade.map(convertGradesToPoints);
+const pointsTotal = gpaPoints.reduce(function (total,item) {
+  return total + item;
+})
+const gpa = pointsTotal / gpaPoints.length;
+
+const words = ["watermelon", "peach", "apple", "tomato", "grape"];
+const shortWords = words.filter(function (word) {
+  return word.length < 6;
+});
+
+const myArray = [12, 34, 21, 54];
+const luckyNumber = 21;
+let luckyIndex = myArray.indexOf(luckyNumber);
